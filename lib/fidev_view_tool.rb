6f6d2f7c93dd6ce083da49a83c8d5dae615e5282
Fillipe Massuda <fi.massuda@gmail.com>
2017-04-15 21:46:49 -0300

    Dev tool initial commit

---
+++ lib/fidev_view_tool.rb
@@ -0,0 +1,6 @@
+require "fidev_view_tool/version"
+require "fidev_view_tool/renderer"
+
+module FidevViewTool
+  # Your code goes here...
+end
---
+++ lib/fidev_view_tool/version.rb
@@ -0,0 +1,3 @@
+module FidevViewTool
+  VERSION = "0.1.0"
+end
